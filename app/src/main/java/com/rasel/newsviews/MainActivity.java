96ffd400687df9d212bc8383212505caecdee9f3
rasel00x <rasel.00x@gmail.com>
2019-02-15 14:49:51 +0600

    added the splash screen to the app

--- app/src/main/java/com/rasel/newsviews/MainActivity.java
+++ app/src/main/java/com/rasel/newsviews/MainActivity.java
@@ -1,14 +1,29 @@
 package com.rasel.newsviews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private SlidePagerAdapter slidePagerAdapter;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.slideViewPager);
+        mTabLayout = findViewById(R.id.tab_layout);
+
+        slidePagerAdapter = new SlidePagerAdapter(this);
+        viewPager.setAdapter(slidePagerAdapter);
+
+        mTabLayout.setupWithViewPager(viewPager, true);
     }
 }
